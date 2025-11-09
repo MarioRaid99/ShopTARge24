adbf2f059db59657e0c8ec2c658b87dff9080249
MarioRaid99 <33650221+MarioRaid99@users.noreply.github.com>
2025-11-09 20:02:26 +0200

    Bug_fix_2

--- ShopTARge24/ShopTARge24/Program.cs
+++ ShopTARge24/ShopTARge24/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
 builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
 
 builder.Services.AddScoped<IChuckNorrisServices, ChuckNorrisServices>();
+builder.Services.AddHttpClient<IChuckNorrisServices, ChuckNorrisServices>();
 
 builder.Services.AddScoped<IOpenWeatherServices, OpenWeatherServices>();
 
