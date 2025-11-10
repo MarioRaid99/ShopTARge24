6cdc7b654dc083a025e73a4238156eb70b389a51
Opilane TTHK <opilane@tthk.ee>
2025-11-10 10:06:58 +0200

    RealEstate_Test

---
+++ ShopTARge24/ShopTARge24.RealEstateTest/Macros/IMacros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARge24.RealEstateTest.Macros
+{
+    public interface IMacros
+    {
+    }
+}
---
+++ ShopTARge24/ShopTARge24.RealEstateTest/RealEstateTest.cs
@@ -0,0 +1,35 @@
+﻿using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+using System.Threading.Tasks;
+
+namespace ShopTARge24.RealEstateTest
+{
+    public class RealEstateTest : TestBase
+    {
+        [Fact]
+        public async Task ShouldNot_AddEmptyRealEstate_WhenReturnResult()
+        {
+            // Arrange
+            RealEstateDto dto = new()
+            {
+                Area = 120.5,
+                Location = "Test Location",
+                RoomNumber = 3,
+                BuildingType = "Apartment",
+                CreatedAt = DateTime.UtcNow,
+                ModifiedAt = DateTime.UtcNow
+            };
+
+            // Act
+            var result = await Svc<IRealEstateServices>().Create(dto);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        //ShouldNot_GetByIdRealestate_WhenReturnsNotEqual()
+        //Should_GetByIdRealestate_WhenReturnsEqual()
+        //Should_DeleteByIdRealEstate_WhenDeleteRealEstate()
+        //ShouldNot_DeleteByIdRealEstate_WhenDidNotDeleteRealEstate()
+    }
+}
