71957d08c8b84ba72ba9bfcc7b0cdb0c2762d394
Opilane TTHK <opilane@tthk.ee>
2025-12-02 10:49:59 +0200

    SignalRSample
    Learned how to set up SignalR

---
+++ ShopTARge24/SignalRSample/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRSample.Data;
+using SignalRSample.Hubs;
+using SignalRSample.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace SignalRSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<DeathlyHallowsHub> _deathlyHub;
+        private readonly ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger,
+            IHubContext<DeathlyHallowsHub> deathlyHub,
+            ApplicationDbContext context)
+        {
+            _logger = logger;
+            _deathlyHub = deathlyHub;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public async Task<IActionResult> DeathlyHallows(string type)
+        {
+            if (SD.DealthyHallowRace.ContainsKey(type))
+            {
+                SD.DealthyHallowRace[type]++;
+            }
+            await _deathlyHub.Clients.All.SendAsync("updateDealthyHallowCount",
+                SD.DealthyHallowRace[SD.Cloak],
+                SD.DealthyHallowRace[SD.Stone],
+                SD.DealthyHallowRace[SD.Wand]);
+
+            return Accepted();
+        }
+
+        public IActionResult Notification()
+        {
+            return View();
+        }
+        public IActionResult DeathlyHallowRace()
+        {
+            return View();
+        }
+        public IActionResult HarryPotterHouse()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
